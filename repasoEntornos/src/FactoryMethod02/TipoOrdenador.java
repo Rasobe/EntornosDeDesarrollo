5384d7feddf3e283d7e9d5754e01eae202f18e5d
Rasobe <rasoberdiel3@gmail.com>
2022-06-03 00:49:38 +0200

    actualizacoin factorymethod2

--- repasoEntornos/src/FactoryMethod02/TipoOrdenador.java
+++ repasoEntornos/src/FactoryMethod02/TipoOrdenador.java
@@ -1,5 +1,7 @@
 package FactoryMethod02;
 
+// esto es lo mismo que poner un IDENTIFICADOR en el construcor de cada uno de las subclases
+
 public enum TipoOrdenador {
 
 	PC, SERVIDOR, SUPERORDENADOR
